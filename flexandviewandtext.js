a6f7b0c2840a278a9ee5a1582c24a8bb491e4835
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-10-11 13:03:48 +1100

    using flex, view and text

---
+++ flexandviewandtext.js
@@ -0,0 +1,54 @@
+React Native Flex and View and Text
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.body}>
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#0000ff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  view1: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00ff00',
+  },
+  view2: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00ffff',
+  },
+  view3: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff00ff',
+  },
+  text: {
+    color: '#000000',
+    fontSize: 30,
+  },
+});
+export default App;
+
