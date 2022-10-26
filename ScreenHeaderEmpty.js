a13ef45c4fc65907cb0283a63e5e347c63b17d28
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-10-26 13:13:30 +1100

    Removing Header from different screen in React Native

---
+++ ScreenHeaderEmpty.js
@@ -0,0 +1,72 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+
+const Stack = createStackNavigator();
+function screenA({navigation}) {
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B');
+    //navigation.replace('Screen_B');
+  };
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen A</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({pressed}) => [
+          {backgroundColor: pressed ? '#ddd' : '#00ff00'},
+        ]}>
+        <Text style={styles.text}>Go To screenB</Text>
+      </Pressable>
+    </View>
+  );
+}
+function screenB({navigation}) {
+  const onPressHandler = () => {
+    //navigation.navigate('Screen_A');
+    navigation.goBack();
+  };
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen B</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({pressed}) => [
+          {backgroundColor: pressed ? '#ddd' : '#00ff00'},
+        ]}>
+        <Text style={styles.text}>Go Back To screenA</Text>
+      </Pressable>
+    </View>
+  );
+}
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          header: () => null,
+        }}>
+        <Stack.Screen
+          name="Screen_A"
+          component={screenA}
+          //options={{header: () => null}}
+        />
+        <Stack.Screen name="Screen_B" component={screenB} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 40,
+  },
+});
+
+export default App;
+
