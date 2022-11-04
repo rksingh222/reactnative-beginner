a116373a0f6b94553244b47330d2d163ecfd1e44
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-11-04 18:02:48 +1100

    bottom material tab navigation

---
+++ bottommaterialtabnavigation.js
@@ -0,0 +1,70 @@
+//1. npm install @react-navigation/material-bottom-tabs react-native-paper react-native-vector-icons
+//.  import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabsimport
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  BottomTabBar,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+import ScreenA from './ScreenA';
+import ScreenB from './ScreenB';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Screen_A') {
+              iconName = 'autoprefixer';
+              size = focused ? 25 : 20;
+              color = focused ? '#f0f' : '#555';
+            } else if (route.name === 'Screen_B') {
+              iconName = 'btc';
+              size = focused ? 25 : 20;
+              color = focused ? '#f0f' : '#555';
+            }
+            return <FontAwesome5 name={iconName} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#f0f',
+          inactiveTintColor: '#555',
+          activeBackgroundColor: '#0ff',
+          inativeBackgroundColor: '#999',
+          showLabel: true,
+          labelStyle: {fontSize: 14},
+        }}
+        activeColor="#f0edf6"
+        inactiveColor="#3e2465"
+        barStyle={{backgroundColor: '#694fad'}}>
+        <Tab.Screen
+          name="Screen_A"
+          component={ScreenA}
+          options={{tabBarBadge: 3}}
+        />
+        <Tab.Screen name="Screen_B" component={ScreenB} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 40,
+  },
+});
+
+export default App;
+
