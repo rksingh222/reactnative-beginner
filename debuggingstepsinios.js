672d6aa73b6af963906dc7885d80cb212279160f
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2022-11-15 16:05:26 +1100

    debugging step in iOS React Native

---
+++ debuggingstepsinios.js
@@ -0,0 +1,25 @@
+/* Debugging in VS code
+
+1. Plug in section (View -> Extension)
+2. React Native tools (Debugging and integrated command)
+3. After Installing click on triangular button at the left corner 
+4. It will display Run And Debug create a launch.json file
+5. This can done with ctrl or command + shift + d
+6. Click on launch.json file
+7. Select on environment ( it should have react native)
+8. If you don’t find it close all the open file on vs code
+9. Then try to click on launch.json file
+10. Select react native
+11. Set attach to packager and remove all the checkpoints
+12. At the top you will see run and debug  and attach to packager
+13. Its not debugging 
+14. So I went to pod file inside iOS set this
+15. :hermes_enabled=>false
+16.  Pod install on terminal inside iOS folder
+17. First in terminal press d for developer mode
+18. Then d in simulator
+19. Debug in chrome 
+20. As you do that you will see the debugger working
+21. Terminate the debugger by selecting 6th button at the top . First is blue color triangle
+
+*/
